3ac7ed7771c02897a65df617240545e41eb4434f
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-11 22:03:16 +0300

    Добавьте файлы проекта.

---
+++ lab7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            calcSum(a, b);
+            Console.ReadKey();
+        }
+    }
+}
